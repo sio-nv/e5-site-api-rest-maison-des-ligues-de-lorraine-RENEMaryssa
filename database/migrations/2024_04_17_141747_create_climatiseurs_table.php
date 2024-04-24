114f4adc818fa1d6a010ff5ff3b234e2dea751c4
RENEMaryssa <maryssa.rene972@icloud.com>
2024-04-24 15:38:13 +0000

    epreuve E5

---
+++ database/migrations/2024_04_17_141747_create_climatiseurs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use function Ramsey\Uuid\v1;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('climatiseurs', function (Blueprint $table) {
+            $table->id();
+            $table->text('marque');
+            $table->text('modele');
+            $table->integer('puissance');
+            $table->integer('surfaceMin');
+            $table->integer('surfaceMax');
+            $table->integer('numSalle');
+
+            $table->foreign('numSalle')->references('numeroSalle')->on('salles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('climatiseurs');
+    }
+};
